a84a0a3e83ae774d5138e48111a6f2b151ac7744
Susanne1208 <Susanne_olsen@live.dk>
2018-05-01 17:22:56 +0200

    Redone some code in file_server, fails on unhandled exception

--- file_server/file_server.cs
+++ file_server/file_server.cs
@@ -13,7 +13,7 @@ namespace Application
 		/// </summary>
 		private const int BUFSIZE = 1000;
 		private const string APP = "FILE_SERVER";
-
+		Transport t1;
 		/// <summary>
 		/// Initializes a new instance of the <see cref="file_server"/> class.
 		/// </summary>
@@ -24,20 +24,21 @@ namespace Application
 			string filePath = string.Empty;
 			string fileName = string.Empty;
 			long fileSizeLong = 0;  
-			byte[] tempBuf = 0;
+			byte[] tempBuf = new byte[BUFSIZE];
 
 			//string fileSizeStr = string.Empty;
 
-			Transport t1 = new Transport (BUFSIZE, APP);
+			t1 = new Transport (BUFSIZE, APP);
 			Console.WriteLine("Server started");
 
-			filePath = t1.receive(ref tempBuf); // wait for client input
-
-			fileName = LIB.extractFileName ();
+			t1.receive (ref tempBuf); 
+			filePath = Encoding.ASCII.GetString(tempBuf);
+			fileName = LIB.extractFileName (filePath);
 
 			Console.WriteLine($"Server looking for file {fileName}");
+
 			fileSizeLong = LIB.check_File_Exists (fileName);
-			sendFile (fileName, fileSizeLong, t1);
+			sendFile (filePath, fileSizeLong, t1);
 		}
 
 		/// <summary>
@@ -52,26 +53,29 @@ namespace Application
 		/// <param name='tl'>
 		/// Tl.
 		/// </param>
-		private void sendFile(String fileName, long fileSize, Transport transport)
+		private void sendFile(String filePath, long fileSize, Transport transport)
 		{
 			// TO DO Your own code
 			// Transport.send(byte[] buf, int size)
 
 			Byte[] bufferServer = new Byte[BUFSIZE]; 
+			string fileName = String.Empty;
 
+			fileName = LIB.extractFileName (filePath);
 			Console.WriteLine (fileName);
 
-			FileStream Fs = new FileStream (fileName, FileMode.Open, FileAccess.Read);
+			FileStream Fs = new FileStream (filePath, FileMode.Open, FileAccess.Read);
 
 			int bytesRead = Fs.Read(bufferServer, 0, BUFSIZE); //Der bliver læst fra fileName, puttes ind i bufferserveren og må max læse 1000 bytes(BUFSIZE)
-			io.Write (bufferServer, 0, bytesRead);
+			transport.send(bufferServer, bytesRead);
+
 			//Whileloop fortsætter, så længe der er bytes at sende (fra fil)
 			while(bytesRead > 0)
 			{
 				bytesRead = Fs.Read(bufferServer, 0, BUFSIZE);
-				io.Write (bufferServer, 0, bytesRead);
-
+				transport.send(bufferServer, bytesRead);
 			}
+
 			Console.WriteLine ("File sent!");
 			Fs.Close ();
 		}
