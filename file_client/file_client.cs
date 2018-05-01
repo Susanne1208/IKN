5ce91ff12206636fa5aa98482b751b0c4f4ae773
Susanne1208 <Susanne_olsen@live.dk>
2018-05-01 16:00:15 +0200

    Added TCP code in file_client

--- file_client/file_client.cs
+++ file_client/file_client.cs
@@ -29,6 +29,40 @@ namespace Application
 	    private file_client(String[] args)
 	    {
 	    	// TO DO Your own code
+			string filePath = "";
+			long fileSize = 0;
+
+			TcpClient ClientSocket = new TcpClient ();
+			try
+			{
+				ClientSocket.Connect(args[0], PORT);
+				Console.WriteLine("Connected to {0}, port {1}", args[0], PORT);
+			}
+			catch
+			{
+				Console.WriteLine ("No connection");
+			}
+			NetworkStream serverStream = ClientSocket.GetStream();
+
+			//Saving requested file in variable filePath
+			filePath = args[1];
+			Console.WriteLine ("Requesting file...");
+			LIB.writeTextTCP (serverStream, filePath);
+
+			fileSize = LIB.getFileSizeTCP (serverStream);
+
+			//If file does not exist, keep asking for existing file
+			while (fileSize == 0) {
+				Console.WriteLine ("!File not found!");
+				Console.WriteLine ("Enter new file: ");
+				filePath = Console.ReadLine ();
+
+				Console.WriteLine ("Requesting file...");
+				LIB.writeTextTCP (serverStream, filePath);
+				fileSize = LIB.getFileSizeTCP (serverStream);
+			}
+			Console.WriteLine ("Receiving file...");
+			receiveFile (filePath, serverStream);
 	    }
 
 		/// <summary>
@@ -43,6 +77,30 @@ namespace Application
 		private void receiveFile (String fileName, Transport transport)
 		{
 			// TO DO Your own code
+			string fileName = string.Empty;
+			string fileDirectory = string.Empty;
+			long fileSize = 0;
+			int bytesRead = 0;
+			byte[] buffer = new byte[BUFSIZE];
+
+			//Create directory for file
+			fileDirectory = "/root/Desktop/ServerFiles/";
+			Directory.CreateDirectory (fileDirectory);
+
+			fileName = LIB.extractFileName(filePath);
+			FileStream Fs = new FileStream (fileDirectory + fileName, FileMode.OpenOrCreate, FileAccess.Write);
+			Console.WriteLine ("Reading file " + fileName + "...");
+
+			//Writes into file as long as it receives bytes, bytesRead>0
+			do
+			{
+				bytesRead = io.Read (buffer, 0, BUFSIZE);
+				Fs.Write(buffer, 0, bytesRead);
+				Console.WriteLine("Read bytes: " + bytesRead.ToString());
+			}while (bytesRead > 0);
+
+			//Lukker fil
+			Fs.Close ();
 		}
 
 		/// <summary>
@@ -53,6 +111,7 @@ namespace Application
 		/// </param>
 		public static void Main (string[] args)
 		{
+			Console.WriteLine ("Client starts...");
 			new file_client(args);
 		}
 	}
