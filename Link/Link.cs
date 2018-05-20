7ea5f02bee9b3319bc0e9a524c57e80608f53601
Susanne1208 <Susanne_olsen@live.dk>
2018-05-20 21:55:48 +0200

    Stop nu

--- Link/Link.cs
+++ Link/Link.cs
@@ -126,7 +126,8 @@ namespace Linklaget
 			}
 
 			var tempByte = (byte)serialPort.ReadByte();
-			Console.WriteLine (tempByte);
+			string tempString = tempByte.ToString;
+			Console.WriteLine (tempString);
 			while (tempByte != DELIMITER)
 			{
 				if (tempByte == (byte)'B')
